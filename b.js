46be6cba4cd5ddcc12eb9b3f0e0fd37221f7db48
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-05-04 15:56:04 +0800

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,15 @@
 var w=window.location.host;if(w=="clt.awaliny.top"){location.replace("https://collections.awaliny.top/")}
+window.addEventListener('load', async () => {
+    navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`)
+        .then(async reg => {
+            //安装成功，建议此处强刷新以立刻执行SW
+            if (window.localStorage.getItem('install') != 'true') {
+                window.localStorage.setItem('install', 'true');
+                setTimeout(() => {
+                    window.location.search = `?time=${new Date().getTime()}`
+                }, 1000)
+            }
+        }).catch(err => {
+            //安装失败，错误信息会由err传参
+        })
+});
